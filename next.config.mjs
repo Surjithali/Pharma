80343a049a34620a382f250bef2f38ce307ae45b
Surjith <surjithali047@gmail.com>
2026-07-01 18:48:18 +0400

    Cheque add opion

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
 
   // ✅ Keeps type validation warnings unblocked during active development sessions
